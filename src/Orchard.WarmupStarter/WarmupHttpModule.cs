54fd8f87e08e09142299811542782f95a8259598
NoReply Amazon <no-reply@amazon.com>
2024-11-24 03:48:40 +0000

    Revert "[Commit] Iteration 17 #errors = 272146 <= 272145."
    This reverts commit 9473bdfe0f71f0d63ba62045a90a4a6d159e93a7.

--- src/Orchard.WarmupStarter/WarmupHttpModule.cs
+++ src/Orchard.WarmupStarter/WarmupHttpModule.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Web;
-using AspNetCoreHttpContext = Microsoft.AspNetCore.Http.HttpContext;
 
 namespace Orchard.WarmupStarter {
     public class WarmupHttpModule : IHttpModule {
