74c4071ae66a2ef7d86999cd61fe904562ec8c24
NoReply Amazon <no-reply@amazon.com>
2024-11-24 05:07:46 +0000

    [Commit] Iteration 154 #errors = 272144 <= 272144.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Orchard.WarmupStarter/WarmupUtility.cs][CS0433][44,42] The type 'HttpRequest' exists in both 'Microsoft.AspNetCore.SystemWebAdapters, Version=1.4.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' and 'System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'``.

--- src/Orchard.WarmupStarter/WarmupUtility.cs
+++ src/Orchard.WarmupStarter/WarmupUtility.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 using System.Web;
 using System.Web.Hosting;
+using HttpRequest = System.Web.HttpRequest;
 
 namespace Orchard.WarmupStarter {
     public static class WarmupUtility {
