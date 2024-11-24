f3c7258eef2ca1c5185997174d2f745321f815d1
NoReply Amazon <no-reply@amazon.com>
2024-11-24 04:17:43 +0000

    Revert "[Commit] Iteration 71 #errors = 272146 <= 272145."
    This reverts commit 227b6fe0b264b67c25b3ced5ad065a506fbcfb2f.

--- src/Orchard.WarmupStarter/Starter.cs
+++ src/Orchard.WarmupStarter/Starter.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SystemWebAdapters;
+using System.Web;
 
 namespace Orchard.WarmupStarter {
     public class Starter<T> where T : class {
@@ -33,15 +32,15 @@ namespace Orchard.WarmupStarter {
             _endRequest = endRequest;
             }
 
-        public void OnApplicationStart(HttpApplication application) {
+        public void OnApplicationStart(System.Web.HttpApplication application) {
             LaunchStartupThread(application);
             }
 
-        public void OnBeginRequest(HttpApplication application) {
+        public void OnBeginRequest(System.Web.HttpApplication application) {
             // Initialization resulted in an error
             if (_error != null) {
                 // Save error for next requests and restart async initialization.
-                // Note: The reason we have to retry the initialization is that the
+                // Note: The reason we have to retry the initialization is that the 
                 //       application environment may change between requests,
                 //       e.g. App_Data is made read-write for the AppPool.
                 bool restartInitialization = false;
@@ -70,7 +69,7 @@ namespace Orchard.WarmupStarter {
             }
         }
 
-        public void OnEndRequest(HttpApplication application) {
+        public void OnEndRequest(System.Web.HttpApplication application) {
             // Only notify if the initialization has successfully completed
             if (_initializationResult != null) {
                 _endRequest(application, _initializationResult);
@@ -80,7 +79,7 @@ namespace Orchard.WarmupStarter {
         /// <summary>
         /// Run the initialization delegate asynchronously in a queued work item
         /// </summary>
-        public void LaunchStartupThread(HttpApplication application) {
+        public void LaunchStartupThread(System.Web.HttpApplication application) {
             // Make sure incoming requests are queued
             WarmupHttpModule.SignalWarmupStart();
 
