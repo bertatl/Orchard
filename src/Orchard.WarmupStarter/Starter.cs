227b6fe0b264b67c25b3ced5ad065a506fbcfb2f
NoReply Amazon <no-reply@amazon.com>
2024-11-24 04:17:43 +0000

    [Commit] Iteration 71 #errors = 272146 <= 272145.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Orchard.WarmupStarter/Starter.cs][CS0433][39,47] The type 'HttpApplication' exists in both 'Microsoft.AspNetCore.SystemWebAdapters, Version=1.4.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' and 'System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'``.

--- src/Orchard.WarmupStarter/Starter.cs
+++ src/Orchard.WarmupStarter/Starter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
-using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SystemWebAdapters;
 
 namespace Orchard.WarmupStarter {
     public class Starter<T> where T : class {
@@ -32,15 +33,15 @@ namespace Orchard.WarmupStarter {
             _endRequest = endRequest;
             }
 
-        public void OnApplicationStart(System.Web.HttpApplication application) {
+        public void OnApplicationStart(HttpApplication application) {
             LaunchStartupThread(application);
             }
 
-        public void OnBeginRequest(System.Web.HttpApplication application) {
+        public void OnBeginRequest(HttpApplication application) {
             // Initialization resulted in an error
             if (_error != null) {
                 // Save error for next requests and restart async initialization.
-                // Note: The reason we have to retry the initialization is that the 
+                // Note: The reason we have to retry the initialization is that the
                 //       application environment may change between requests,
                 //       e.g. App_Data is made read-write for the AppPool.
                 bool restartInitialization = false;
@@ -69,7 +70,7 @@ namespace Orchard.WarmupStarter {
             }
         }
 
-        public void OnEndRequest(System.Web.HttpApplication application) {
+        public void OnEndRequest(HttpApplication application) {
             // Only notify if the initialization has successfully completed
             if (_initializationResult != null) {
                 _endRequest(application, _initializationResult);
@@ -79,7 +80,7 @@ namespace Orchard.WarmupStarter {
         /// <summary>
         /// Run the initialization delegate asynchronously in a queued work item
         /// </summary>
-        public void LaunchStartupThread(System.Web.HttpApplication application) {
+        public void LaunchStartupThread(HttpApplication application) {
             // Make sure incoming requests are queued
             WarmupHttpModule.SignalWarmupStart();
 
